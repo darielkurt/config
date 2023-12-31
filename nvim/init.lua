47796ea415663188ba07b9088b08839d39e5d1b8
Kurt Bermudez <kurtdariel@gmail.com>
2023-12-31 13:09:16 +0800

    * added mapping for auto format

--- nvim/init.lua
+++ nvim/init.lua
@@ -466,6 +466,7 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat' })
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
