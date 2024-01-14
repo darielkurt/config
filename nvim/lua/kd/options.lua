b34a1cc847ecf9ada0793657511eec1cfacc43b6
Kurt Bermudez <kurtdariel@gmail.com>
2024-01-14 19:10:11 +0800

    + added next greatest remap ever"

--- nvim/lua/kd/options.lua
+++ nvim/lua/kd/options.lua
@@ -90,3 +90,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
