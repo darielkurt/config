eb073064ddf7c707aff7acee2f804f9da27b170b
Kurt Bermudez <kurtdariel@gmail.com>
2024-03-26 22:38:48 +0800

    tab spacings

--- nvim/lua/kd/options.lua
+++ nvim/lua/kd/options.lua
@@ -84,3 +84,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.opt.scrolloff = 8
+
+-- indent
+vim.o.tabstop = 2
+vim.bo.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.bo.shiftwidth = 2
+vim.o.autoindent = true
+vim.o.autoread = true
+vim.bo.autoindent = true
+vim.o.expandtab = true
+vim.bo.expandtab = true
