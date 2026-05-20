fa2934e6e81d9542a509557c23b317e51bbb3389
Kurt Bermudez <kurt@wizy.io>
2026-05-20 13:24:29 +0800

    * fixed dart lsp

--- nvim/lua/kd/flutter-tools.lua
+++ nvim/lua/kd/flutter-tools.lua
@@ -1,5 +1,7 @@
 require("flutter-tools").setup({
 	lsp = {
-		on_attach = require("kd.on-attach")
+		on_attach = require("kd.on-attach"),
+
+		cmd = { "/Users/kurtdariel/.asdf/installs/flutter/3.32.5/bin/dart", "language-server" },
 	},
 })
